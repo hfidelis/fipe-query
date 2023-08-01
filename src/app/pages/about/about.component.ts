9d07453bda2620cb8d97e772394e9b96eff1b3a8
Heitor Fidelis <heitorc88@gmail.com>
2023-08-01 02:21:55 -0300

    feat: added about page component

--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
-
+import { faLink, faCircleInfo, faTable, faHandshakeSimple, IconDefinition } from '@fortawesome/free-solid-svg-icons';
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.css']
 })
 export class AboutComponent {
-
+  faLink: IconDefinition = faLink;
+  faTable: IconDefinition = faTable;
+  faHandshakeSimple: IconDefinition = faHandshakeSimple;
+  faCircleInfo: IconDefinition = faCircleInfo;
 }
