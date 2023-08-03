6b90b42e5561d0b2422962f4f6627349f58baab2
Heitor Fidelis <heitorc88@gmail.com>
2023-08-02 23:35:23 -0300

    feat: finish services methods

--- src/app/modules/query/services/fipe.service.ts
+++ src/app/modules/query/services/fipe.service.ts
@@ -1,16 +1,42 @@
-import { Injectable } from '@angular/core';
-
-import { Observable } from 'rxjs';
-
 import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { map, Observable } from 'rxjs';
+import { environment } from 'src/environments/environment.development';
+
+import { FipePattern } from '../models/FipePattern';
+import { Vehicle } from '../models/Vehicle';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FipeService {
 
+  private readonly baseApiUrl: string = environment.baseApiUrl
+
   constructor(
     private http: HttpClient
   ) { }
-  
+
+  getBrands(type: string): Observable<Array<FipePattern>> {
+    const url: string = `${this.baseApiUrl}${type}/marcas`;
+    return this.http.get<Array<FipePattern>>(url);
+  }
+
+  getModels(type: string, brandCode: number): Observable<any> {
+    const url: string = `${this.baseApiUrl}${type}/marcas/${brandCode}/modelos`;
+    return this.http.get<any>(url).pipe(
+      map(data => data.modelos)
+    );
+  }
+
+  getModelYears(type: string, brandCode: number, modelCode: number): Observable<Array<FipePattern>> {
+    const url: string = `${this.baseApiUrl}${type}/marcas/${brandCode}/modelos/${modelCode}/anos`;
+    return this.http.get<Array<FipePattern>>(url);
+  }
+
+  getModelDetails(type: string, brandCode: number, modelCode: number, yearCode: string): Observable<Vehicle> {
+    const url: string = `${this.baseApiUrl}${type}/marcas/${brandCode}/modelos/${modelCode}/anos/${yearCode}`;
+    return this.http.get<Vehicle>(url);
+  }
+
 }
