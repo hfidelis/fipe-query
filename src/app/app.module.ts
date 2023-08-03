ffbb79a7c7f9583d816a297c2b02322a304637d1
Heitor Fidelis <heitorc88@gmail.com>
2023-08-02 23:36:58 -0300

    refactor: ordered imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
-import { HomeComponent } from './pages/home/home.component';
-import { AboutComponent } from './pages/about/about.component';
+import { HeaderComponent } from './components/header/header.component';
 import { QueryModule } from './modules/query/query.module';
-import { HomeSliderSectionComponent } from './pages/home/components/home-slider-section/home-slider-section.component';
+import { AboutComponent } from './pages/about/about.component';
 import { HomeModelsSectionComponent } from './pages/home/components/home-models-section/home-models-section.component';
+import { HomeSliderSectionComponent } from './pages/home/components/home-slider-section/home-slider-section.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
   declarations: [
