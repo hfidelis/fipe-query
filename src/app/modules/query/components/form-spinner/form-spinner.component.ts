75e9cc9518262708977a63e4bae45b742cd571cb
Heitor Fidelis <heitorc88@gmail.com>
2023-08-05 17:23:01 -0300

    feat: finished form-spinner component settings

--- src/app/modules/query/components/form-spinner/form-spinner.component.ts
+++ src/app/modules/query/components/form-spinner/form-spinner.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { IconDefinition, faGear } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-form-spinner',
@@ -6,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./form-spinner.component.css']
 })
 export class FormSpinnerComponent {
-
+  faGear: IconDefinition = faGear;
 }
