ea923b056ab9065c5713f01a558b1393a671272c
Heitor Fidelis <heitorc88@gmail.com>
2023-08-07 21:01:36 -0300

    feat: query page finished

--- src/app/modules/query/pages/query/query.component.ts
+++ src/app/modules/query/pages/query/query.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+import { QueryValue } from '../../models/QueryValue';
+
 @Component({
   selector: 'app-query',
   templateUrl: './query.component.html',
@@ -7,4 +9,32 @@ import { Component } from '@angular/core';
 })
 export class QueryComponent {
 
+  showForm: boolean = true;
+  isLoading: boolean = false;
+  isData: boolean = false;
+  dataValue!: QueryValue;
+
+  constructor() {}
+
+  handleSubmit($event: QueryValue) {
+
+    this.dataValue = $event;
+    // Destroy query-form component;
+    this.showForm = false;
+    // Init form-spinner component;
+    this.isLoading = true;
+
+    setTimeout(() => {
+      // Destroy form-spinner component;
+      this.isLoading = false;
+      this.isData = true;
+    }, 3500);
+
+  }
+
+  onGoBack() {
+    this.isData = false;
+    this.showForm = true;
+  }
+  
 }
